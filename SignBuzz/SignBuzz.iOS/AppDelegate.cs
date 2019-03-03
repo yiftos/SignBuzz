76cebd56d8eeeff8724d7010b0cfab584ef575df
yoavbt <yoav.bar.sus@gmail.com>
2019-03-03 17:30:12 +0200

    adding auth in apple take1

--- SignBuzz/SignBuzz.iOS/AppDelegate.cs
+++ SignBuzz/SignBuzz.iOS/AppDelegate.cs
@@ -4,6 +4,8 @@ using System.Linq;
 
 using Foundation;
 using UIKit;
+using Microsoft.WindowsAzure.MobileServices;
+using System.Threading.Tasks;
 
 namespace SignBuzz.iOS
 {
@@ -11,7 +13,7 @@ namespace SignBuzz.iOS
     // User Interface of the application, as well as listening (and optionally responding) to 
     // application events from iOS.
     [Register("AppDelegate")]
-    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate, IAuthenticate
     {
         //
         // This method is invoked when the application has loaded and is ready to run. In this 
@@ -23,9 +25,62 @@ namespace SignBuzz.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
+            App.Init(this);
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
+        }
+        // Define a authenticated user.
+        private MobileServiceUser user;
+
+        public async Task<bool> Authenticate(bool google)
+        {
+            var success = false;
+            var message = string.Empty;
+            try
+            {
+                if (google)
+                {
+                    // Sign in with Facebook login using a server-managed flow.
+                    user = await MainUserManager.DefaultManager.CurrentClient.LoginAsync(UIApplication.SharedApplication.KeyWindow.RootViewController,
+                        MobileServiceAuthenticationProvider.Google, "signbuzz");
+                    if (user != null)
+                    {
+                        App.user = user;
+                        message = string.Format("you are now signed-in as {0}.",
+                            user.UserId);
+                        success = true;
+                    }
+                }
+                else
+                {
+                    // Sign in with Facebook login using a server-managed flow.
+                    user = await MainUserManager.DefaultManager.CurrentClient.LoginAsync(UIApplication.SharedApplication.KeyWindow.RootViewController,
+                        MobileServiceAuthenticationProvider.Facebook, "signbuzz");
+                    if (user != null)
+                    {
+                        App.user = user;
+                        message = string.Format("you are now signed-in as {0}.",
+                            user.UserId);
+                        success = true;
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+            }
+
+            // Display the success or failure message.
+            UIAlertView avAlert = new UIAlertView("Sign-in result", message, null, "OK", null);
+            avAlert.Show();
+
+            return success;
+        }
+        public override bool OpenUrl(UIApplication app, NSUrl url, NSDictionary options)
+        {
+            return MainUserManager.DefaultManager.CurrentClient.ResumeWithURL(url);
         }
     }
 }
